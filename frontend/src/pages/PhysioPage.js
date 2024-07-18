7b06454927ff3dd4622d586fa7109978cba2b66a
JosephNjorog <njorojoe11173@gmail.com>
2024-07-18 13:01:25 +0300

    feat: Add PhysioPage component for displaying physio information

---
+++ frontend/src/pages/PhysioPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import Physio from '../components/Physio/Physio';
+
+const PhysioPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Physio />
+    </div>
+  );
+};
+
+export default PhysioPage;
