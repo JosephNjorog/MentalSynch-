a67634a0b241ecb199cfc505aae765b216323cf3
JosephNjorog <njorojoe11173@gmail.com>
2024-07-18 13:05:09 +0300

    refactor: Update HomePage component to include Navbar

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <h1>Welcome to MentalSynch</h1>
+        <p>Your mental health companion.</p>
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;
