a3690bd5f1fe6998d66a0b424faf6bdd3485772c
JosephNjorog <njorojoe11173@gmail.com>
2024-07-18 13:17:01 +0300

    Added Dashboard component with styles

---
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Dashboard.css';
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <h1>Welcome to your Dashboard</h1>
+      {/* Dashboard content will be added here */}
+    </div>
+  );
+};
+
+export default Dashboard;
