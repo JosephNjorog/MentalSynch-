83fa91b0fd2bcd8a974cff804be77919d0e2814c
JosephNjorog <njorojoe11173@gmail.com>
2024-07-18 13:14:14 +0300

    refactor: Update Physio component to include Physiotherapy content

---
+++ frontend/src/components/Physio/Physio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Physio.css';
+
+const Physio = () => {
+  return (
+    <div className="physio">
+      <h1>Physiotherapy Section</h1>
+      {/* Physiotherapy content will be added here */}
+    </div>
+  );
+};
+
+export default Physio;
