0f10ebfe37a61c4f0c2a745b44c74933f8dd94a5
JosephNjorog <njojoe11173@gmail.com>
2024-11-14 23:45:17 +0300

    feat: Add health calculation utilities for BMI and BMR

---
+++ mental-frontend/src/utils/healthCalculations.js
@@ -0,0 +1,12 @@
+export const calculateBMI = (weight, height) => {
+    return (weight / (height * height)).toFixed(2);
+  };
+  
+  export const calculateBMR = (weight, height, age, gender) => {
+    if (gender === 'male') {
+      return (88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)).toFixed(2);
+    } else {
+      return (447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)).toFixed(2);
+    }
+  };
+  
