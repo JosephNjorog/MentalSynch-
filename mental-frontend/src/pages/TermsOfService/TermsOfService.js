b312637ed208a206fad6fcdf7ddfa5878df280fa
JosephNjorog <njojoe11173@gmail.com>
2024-11-14 23:52:26 +0300

    feat: Add Terms of Service page with styling

---
+++ mental-frontend/src/pages/TermsOfService/TermsOfService.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TermsOfService.css';
+
+const TermsOfService = () => {
+  return (
+    <div className="terms-of-service">
+      <h2>Terms of Service</h2>
+      <p>These are our terms of service. FUCK YOU</p>
+    </div>
+  );
+};
+
+export default TermsOfService;
