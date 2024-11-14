1621feae9e808234d2a3d6e48e6a0d2f96929a02
JosephNjorog <njojoe11173@gmail.com>
2024-11-14 23:39:14 +0300

    feat: Add ExerciseCard component for displaying exercise details

---
+++ mental-frontend/src/components/VRTherapy/ExerciseCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './VRTherapy.css';
+
+const ExerciseCard = ({ exercise }) => {
+  return (
+    <div className="exercise-card">
+      <h3>{exercise.title}</h3>
+      <p>{exercise.description}</p>
+    </div>
+  );
+};
+
+export default ExerciseCard;
