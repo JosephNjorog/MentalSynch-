98d8c0085346bde5f4eba55306760b4fb152b518
JosephNjorog <njojoe11173@gmail.com>
2024-11-15 00:14:22 +0300

    feat: Add Privacy Policy page with styling

---
+++ mental-frontend/src/PrivacyPolicy/PrivacyPolicy.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PrivacyPolicy.css';
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="privacy-policy">
+      <h2>Privacy Policy</h2>
+      <p>This is our privacy policy.</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
